1df8c6b88e251b8bf1032820aa802f3a19006115
klosorio10 <kl.osorio10@uniandes.edu.co>
2017-02-23 10:59:49 -0500

    Primer ejercicio monogo+express

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/mongoBDT';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.close();
+});	
+
+// se establece el motor del front
+app.set('view engine', 'pug');
+// Links a las carpetas de vista y estatica
+app.set('views', './views');
+app.use(express.static('public'));
+
+
+app.listen(3001, function(){
+	console.log("Starting on port 3001");
+});
